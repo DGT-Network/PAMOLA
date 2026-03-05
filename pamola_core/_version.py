6d02275c30b5ff743ae7cc79096c3e9d5e55f208
TITANCORP\thuan.dang <TITANCORP\thuan.dang>
2026-03-05 10:11:43 +0700

    update dependency for suport 3.10

--- pamola_core/_version.py
+++ pamola_core/_version.py
@@ -1,1 +1,7 @@
-__version__ = "0.0.0"
+# pamola_core/_version.py
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pamola-core")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
